0484e74e2746c3534b06e2d31671642df746f206
Matt Smith <psykzz@users.noreply.github.com>
2018-11-26 15:31:49 +0000

    Update watch command

--- plugins/server-query-watch/index.js
+++ plugins/server-query-watch/index.js
@@ -6,18 +6,46 @@ const async = require('async');
 const sq = new SourceQuery(5000); // 5000ms timeout
 
 const WATCH_INTERVAL = 3000;
-let watchId;
+let progressCounter = 0;
 let messageId;
+
+function GetInfo(ip, port, msg, counter) {
+    if (!progressCounter !== counter) {
+        return;
+    }
+    sq.open(ip, port);
+    async.parallel([
+        (cb) => sq.getInfo(cb),
+        (cb) => sq.getPlayers(cb),
+        (cb) => sq.getRules(cb)
+    ], (err, results) => {
+        // Close connection
+        sq.close(() => {
+            if (err) {
+                setTimeout(() => GetInfo(ip, port, msg, counter), WATCH_INTERVAL)
+            }
+            let reply = "```";
+            reply += `\nName: ${results[0].name}\n`
+            reply += `Map: ${results[0].map}\n`
+            reply += `Players: ${results[0].players}/${results[0].maxplayers}\n`
+            results[1].forEach(player => {
+                reply += `\t${player.name} - ${Math.round(player.online / 60 / 60)} hour(s)\n`;
+            });
+            reply += "```"
+            msg.edit(reply);
+            setTimeout(() => GetInfo(ip, port, msg, counter), WATCH_INTERVAL)
+        });
+    });
+}
+
+
 
 module.exports = {
     enabled: true,
     prefix: '!watch',
     args: true,
     callback: (bot, message, host) => {
-        
-        if (watchId) {
-            clearTimeout(watchId);
-        }
+        progressCounter += 1;
 
         let ip, port;
         [ip, port] = host.split(':', 2);
@@ -25,31 +53,7 @@ module.exports = {
         // Placeholder message
         bot.reply(message,  'Working...', -1)
         .then(msg => {
-            messageId = msg;
-            watchId = setInterval(() => {
-                sq.open(ip, port);
-                async.parallel([
-                    (cb) => sq.getInfo(cb),
-                    (cb) => sq.getPlayers(cb),
-                    (cb) => sq.getRules(cb)
-                ], (err, results) => {
-                    sq.close(() => {});
-                    if (err) {
-                        return;
-                    }
-                    let reply = "```";
-                    reply += `\nName: ${results[0].name}\n`
-                    reply += `Map: ${results[0].map}\n`
-                    reply += `Players: ${results[0].players}/${results[0].maxplayers}\n`
-                    results[1].forEach(player => {
-                        reply += `\t${player.name} - ${Math.round(player.online / 60 / 60)} hour(s)\n`;
-                    });
-                    reply += "```"
-                    messageId.edit(reply);
-                })
-            }, WATCH_INTERVAL);
+            WatchServerInfo(ip, port, msg, progressCounter);
         });
-
-        
     }
 }
